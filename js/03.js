e1090f228e78d78c478c46325d6c1781a8428fce
Hao <wanghao8522@gmail.com>
2018-08-01 09:02:26 +1200

    03.尚硅谷_JS基础_js编写位置
    外部文件的引入
    onclick 事件的内部和外部使用

---
+++ js/03.js
@@ -0,0 +1,11 @@
+// alert('我是外部文件');
+
+window.onload=function(){
+
+    var btn =document.getElementById('btn');
+
+    btn.onclick=function(){
+              
+        alert('小子，你真的点了啊，我来自外部文件');
+    };
+};
